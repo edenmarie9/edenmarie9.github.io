811992de8712ccb678abd2438a150b59cbd85cb0
edenmarie9 <80850857+edenmarie9@users.noreply.github.com>
2025-11-03 12:37:39 -0600

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,360 @@
+/*
+Name: Eden-Marie Freiku
+Date created: 2025-08-09
+Date last edited: 2025-03-11
+Version: 4.0
+Description: Homework 3 JS. Providing functionality for the patient form
+*/
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+let slider = document.getElementById("range");
+let output = document.getElementById("range-slider");
+output.innerHTML = slider.value;
+
+slider.oninput = function () 
+{
+    output.innerHTML = this.value;
+};
+
+function validateDob() 
+{
+    dob = document.getElementById("dob");
+    let date = new Date(dob.value);
+    let maxDate = new Date().setFullYear(new Date().getFullYear() - 120);
+
+    if (date > new Date()) {
+        document.getElementById("dob-error").innerHTML = 
+        "Date can't be in the future";
+        dob.value = "";
+        return false;
+    } else if (date < new Date(maxDate)) {
+        document.getElementById("dob-error").innerHTML = 
+        "Date can't be more than 120 years ago";
+        dob.value = "";
+        return false;
+    } else {
+        document.getElementById("dob-error").innerHTML = "";
+        return true;
+    }
+}
+function validateSsn() 
+{
+    const ssn = document.getElementById("ssn").value;
+    const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
+
+    if (!ssnR.test(ssn)) {
+        document.getElementById("ssn-error").innerHTML = 
+        "Please enter a valid SSN";
+        return false;
+    } else {
+        document.getElementById("ssn-error").innerHTML = "";
+        return true;
+    }
+}
+function validateSsn() 
+{
+    const ssn = document.getElementById("ssn").value;
+    const ssnR = /^[0-9]{3}-?[0-9]{2}-?[0-9]{4}$/;
+
+    if (!ssnR.test(ssn)) {
+        document.getElementById("ssn-error").innerHTML = 
+        "Please enter a valid SSN";
+        return false;
+    } else {
+        document.getElementById("ssn-error").innerHTML = "";
+        return true;
+    }
+}
+function validateZcode() 
+{
+    const zipInput = document.getElementById("zcode");
+    let zip = zipInput.value.replace(/[^\d-]/g, "");
+
+    if (!zip) {
+        document.getElementById("zcode-error").innerHTML = 
+        "Zip code can't be blank";
+        return false;
+    }
+
+    if (zip.length > 5) {
+        zip = zip.slice(0, 5) + "-" + zip.slice(5, 9);
+    } else {
+        zip = zip.slice(0, 5);
+    }
+
+    zipInput.value = zip;
+    document.getElementById("zcode-error").innerHTML = "";
+    return true;
+}
+
+function validateEmail() 
+{
+    const emailInput = document.getElementById("email");
+    const email = emailInput.value.trim().toLowerCase();
+    const emailR = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+
+    if (email === "") {
+        document.getElementById("email-error").innerHTML = "Email cannot be blank";
+        return false;
+    } else if (!emailR.test(email)) {
+        document.getElementById("email-error").innerHTML = "Please enter a valid email address";
+        return false;
+    } else {
+        document.getElementById("email-error").innerHTML = "";
+        return true;
+    }
+}
+function validatePhone() 
+{
+    const phoneInput = document.getElementById("phone");
+    let phone = phoneInput.value.replace(/\D/g, ""); 
+
+    if (phone === "") {
+        document.getElementById("phone-error").innerHTML = "Phone number can't be blank";
+        return false;
+    }
+
+    if (phone.length !== 10) {
+        document.getElementById("phone-error").innerHTML = "Please enter a valid 10-digit phone number";
+        return false;
+    }
+
+    phone = phone.slice(0, 3) + "-" + phone.slice(3, 6) + "-" + phone.slice(6, 10);
+    phoneInput.value = phone;
+
+    document.getElementById("phone-error").innerHTML = "";
+    return true;
+}
+function validateUid() 
+{
+    uid = document.getElementById("uid").value.toLowerCase();
+    document.getElementById("uid").value = uid;
+
+    if (uid.length == 0) {
+        document.getElementById("uid-error").innerHTML = 
+        "User ID can't be blank";
+        return false;
+    }
+
+    if (!isNaN(uid.charAt(0))) {
+        document.getElementById("uid-error").innerHTML = 
+        "User ID can't start with a number";
+        return false;
+    }
+
+    let regex = /^[a-zA-Z0-9_-]+$/;
+    if (!regex.test(uid)) {
+        document.getElementById("uid-error").innerHTML = 
+        "User ID can only have letters, numbers, underscores, and dashes";
+        return false;
+    } else if (uid.length < 5) {
+        document.getElementById("uid-error").innerHTML = 
+        "User ID must be at least 5 characters";
+        return false;
+    } else if (uid.length > 30) {
+        document.getElementById("uid-error").innerHTML = 
+        "User ID can't exceed 30 characters";
+        return false;
+    } else {
+        document.getElementById("uid-error").innerHTML = "";
+        return true;
+    }
+}
+function validatePword() 
+{
+    const uid = document.getElementById("uid").value.trim();  
+    const pwordInput = document.getElementById("pword");
+    const pword = pwordInput.value;
+    const errorMessage = [];
+
+    if (pword === "") {
+        document.getElementById("pword-error").innerHTML = "Password can't be blank";
+        return false;
+    }
+
+    if (!pword.match(/[a-z]/)) errorMessage.push("Enter at least one lowercase letter");
+    if (!pword.match(/[A-Z]/)) errorMessage.push("Enter at least one uppercase letter");
+    if (!pword.match(/[0-9]/)) errorMessage.push("Enter at least one number");
+    if (!pword.match(/[!\@#\$%&*\-_\\.+\(\)]/)) errorMessage.push("Enter at least one special character");
+    if (pword.includes(uid)) errorMessage.push("Password can't contain user ID");
+
+    if (errorMessage.length > 0) {
+        document.getElementById("pword-error").innerHTML = errorMessage.join("<br>");
+        return false;
+    }
+
+    document.getElementById("pword-error").innerHTML = "";
+    return true;
+}
+function confirmPword() 
+{
+    pword1 = document.getElementById("pword").value;
+    pword2 = document.getElementById("con_pword").value;
+
+    if (pword1 !== pword2) {
+        document.getElementById("pword2-error").innerHTML = 
+        "Passwords don't match";
+        return false;
+    } else {
+        document.getElementById("pword2-error").innerHTML = 
+        "Passwords match";
+        return true;
+    }
+}
+function reviewInput() {
+    var formcontent = document.getElementById("signup");
+    var formoutput = "<table class='output'><th colspan = '3'> Review Your Information:</th>";
+    for (let i = 0; i < formcontent.length; i++) {
+        if (formcontent.elements[i].value !== "") {
+            switch (formcontent.elements[i].type) {
+                case "checkbox":
+                    if (formcontent.elements[i].checked) {
+                        formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>&#x2713;</td></tr>`;
+                    }
+                    break;
+                case "radio":
+                    if (formcontent.elements[i].checked) {
+                        formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
+                    }
+                    break;
+                default:
+                    formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
+            }
+        }
+    }
+    formoutput += "</table>";
+    document.getElementById("showInput").innerHTML = formoutput;
+}
+function removeReview() {
+    document.getElementById("showInput").innerHTML = "";
+}
+function showAlert() {
+    var alertBox = document.getElementById("alert-box");
+    var closeAlert = document.getElementById("close-alert");
+
+    alertBox.style.display = "block";
+    closeAlert.onclick = function() {
+        alertBox.style.display = "none";
+    };
+}
+function validateEverything() {
+    let valid = true;
+
+    if (!validateFname()) {
+        valid = false;
+    }
+    if (!validateMini()) {
+        valid = false;
+    }
+    if (!validateLname()) {
+        valid = false;
+    }
+    if (!validateDob()) {
+        valid = false;
+    }
+    if (!validateSsn()) {
+        valid = false;
+    }
+    if (!validateAddress1()) {
+        valid = false;
+    }
+    if (!validateCity()) {
+        valid = false;
+    }
+    if (!validateZcode()) {
+        valid = false;
+    }
+    if (!validateEmail()) {
+        valid = false;
+    }
+    if (!validatePhone()) {
+        valid = false;
+    }
+    if (!validateUid()) {
+        valid = false;
+    }
+    if (!validatePword()) {
+        valid = false;
+    }
+    if (!confirmPword()) {
+        valid = false;
+    }
+     if (valid) {
+         document.getElementById("submit").disabled = false;
+     } else{
+        showAlert();
+     }
+ }
+ function validateCity() {
+    const city = document.getElementById("city").value.trim();
+
+    if (!city) {
+        document.getElementById("city-error").innerHTML = "City can't be blank";
+        return false;
+    } else {
+        document.getElementById("city-error").innerHTML = "";
+        return true;
+    }
+}
+function validateFname() {
+    const fname = document.getElementById("fname").value.trim();
+    var namePattern = /^[a-zA-Z'-]+$/;
+    if (fname == "") {
+        document.getElementById("fname-error").innerHTML = "First name field cannot be empty"
+        return false;
+    } else if (fname != "") {
+        if (!fname.match(namePattern)) {
+        document.getElementById("fname-error").innerHTML = "Letters, apostrophes, and dashes only.";
+        return false;
+    } else if (fname.length < 2) {
+        document.getElementById("fname-error").innerHTML = "First name cannot be less than 1 character.";
+        return false;
+    } else if (fname.length > 30) {
+        document.getElementById("fname-error").innerHTML = "First name cannot be more than 30 characters.";
+        return false;
+    } else {
+        document.getElementById("fname-error").innerHTML = "";
+        return true;
+    }
+}
+}
+function validateMini() {
+    const mini = document.getElementById("mini").value;
+    var namePattern = /^[A-Z]+$/;
+
+    mini = mini.toUpperCase();
+    document.getElementById("mini").value = mini;
+
+    if (!mini.match(namePattern)) {
+        document.getElementById("mini-error").innerHTML = 
+        "Middle initial must be a single uppercase letter";
+        return false;
+    } else {
+        document.getElementById("mini-error").innerHTML = "";
+        return true;
+    }
+}
+function validateLname() {
+    const lname = document.getElementById("lname").value.trim();
+    var namePattern = /^[a-zA-Z'-]+$/;
+    if (lname == "") {
+        document.getElementById("lname-error").innerHTML = "Last name field cannot be empty"
+        return false;
+    } else if (lname != "") {
+        if (!lname.match(namePattern)) {
+        document.getElementById("lname-error").innerHTML = "Letters, apostrophes, and dashes only.";
+        return false;
+    } else if (lname.length < 2) {
+        document.getElementById("lname-error").innerHTML = "Last name cannot be less than 1 character.";
+        return false;
+    } else if (lname.length > 30) {
+        document.getElementById("lname-error").innerHTML = "Last name cannot be more than 30 characters.";
+        return false;
+    } else {
+        document.getElementById("lname-error").innerHTML = "";
+        return true;
+    }
+}
+}
